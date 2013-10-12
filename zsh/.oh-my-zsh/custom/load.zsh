d652b6d65b5027866108545dd7efe5e36935e806
JChien <jeffchien13@gmail.com>
2013-10-12 22:20:21 +0800

    update zsh

--- zsh/.oh-my-zsh/custom/load.zsh
+++ zsh/.oh-my-zsh/custom/load.zsh
@@ -1,7 +1,7 @@
 files=()
 files+=(~/.oh-my-zsh/custom/configs/complete.zsh)
 files+=(~/.oh-my-zsh/custom/configs/bindkeys.zsh)
-files+=(~/.oh-my-zsh/custom/configs/myalias.zsh)
+files+=(~/.oh-my-zsh/custom/configs/alias.zsh)
 
 for f in ${files[@]}; do
     if [[ -f "$f" ]]; then
