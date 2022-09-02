da90f13ce7d714f992cff73ad3c43f94c1d0f446
JChien <jeffchien13@gmail.com>
2022-09-02 14:06:46 +0800

    make emacs prettier
    Signed-off-by: JChien <jeffchien13@gmail.com>

--- doom_emacs/.doom.d/config.el
+++ doom_emacs/.doom.d/config.el
@@ -1,26 +1,6 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Jeff Chien"
       user-mail-address "jeffchien13@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; https://github.com/doomemacs/doomemacs/issues/1403#issuecomment-491470949
 (setq doom-theme 'wombat)
 (defun reload-theme (frame)
   (with-selected-frame frame
@@ -29,44 +9,31 @@
       (load-theme 'wombat t))))
 (add-hook 'after-make-frame-functions #'reload-theme)
 
+(setq doom-font (font-spec :family "SauceCodePro Nerd Font Mono" :size 15)
+      doom-variable-pitch-font (font-spec :family "Helvetica" :size 15)
+      doom-big-font (font-spec :family "SauceCodePro Nerd Font Mono" :size 24))
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; show the popup window earlier
-(after! 'which-key
-  (setq which-key-idle-delay 0.2))
-
-;; this fix the suggestion list form which-key is partially covered by status line.
-;; https://github.com/doomemacs/doomemacs/issues/5622
-(setq which-key-allow-imprecise-window-fit nil)
+;; hide / * _ ~ markers
+(setq org-hide-emphasis-markers t)
 
 (use-package! org-media-note
   :hook (org-mode . org-media-note-mode)
   :config
   (setq org-media-note-screenshot-image-dir "./assets/")
   )
+(map! :leader
+      :desc "media note controller"
+      "m v" #'org-media-note-hydra/body)
 
 (use-package! org-super-agenda
   :after org-agenda
@@ -87,9 +54,17 @@
   :config
   (org-super-agenda-mode))
 
-(map! :leader
-      :desc "media note controller"
-      "m v" #'org-media-note-hydra/body)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; show the popup window earlier
+(after! 'which-key
+  (setq which-key-idle-delay 0.2))
+
+;; this fix the suggestion list form which-key is partially covered by status line.
+;; https://github.com/doomemacs/doomemacs/issues/5622
+(setq which-key-allow-imprecise-window-fit nil)
 
 ;; no idea the default RET key doesn't work so I have to map it ot other key
 (map! :map evil-multiedit-mode-map
