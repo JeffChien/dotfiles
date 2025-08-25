0359f91f21b892f2faba684c6e92f9f86c6c2d25
JChien <jeffchien13@gmail.com>
2025-08-25 21:48:47 +0800

    add yazi

--- nix/hosts/m1pro/home-jchien.nix
+++ nix/hosts/m1pro/home-jchien.nix
@@ -93,5 +93,8 @@ in
     ".config/fd" = {
       source = mkOutOfStoreSymlink "${dotfilesDirectory}/fd/nix";
     };
+    ".config/yazi" = {
+      source = mkOutOfStoreSymlink "${dotfilesDirectory}/yazi/nix";
+    };
   };
 }
